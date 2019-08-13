7fee679db84bac4e8f081b4b9a335e91d09a126e
root <root@ssd.lan>
2019-08-13 12:13:13 +0200

    all control transfer reproduced but it still doesn't work

--- hidsetu.py
+++ hidsetu.py
@@ -22,43 +22,36 @@ with usb1.USBContext() as context:
     )
     if handle is None:
         print("Device not present, or user is not allowed to access device.")
-#    if handle.is_kernel_driver_active(INTERFACE) is True:
-#        print("Kernel driver active.")
-#    #if handle.kernelDriverActive(0):
-#    #    handle.detachKernelDriver(0)
-#    time.sleep(0.1)
-    #handle.claimInterface(0)
+    if handle.kernelDriverActive(0):
+        handle.detachKernelDriver(0)
+    time.sleep(0.5e-3)
     handle.resetDevice()
-#    time.sleep(0.1)
-    #  time.sleep(0.5e-3)
-    #handle.controlWrite(0x00, 0x09, 0, 0, b"", timeout=20) # mouse LED off
-    print(handle.controlRead(0x80, 0x00, 0x0000, 0x0000, 0x0002, timeout=20)) # get status
-    print(handle.controlRead(0x80, 0x06, 0x0100, 0x0000, 0x12, timeout=20)) # get descriptor len 0x12
-    print(handle.controlRead(0x80, 0x06, 0x0300, 0x0000, 0xFF, timeout=20)) # get descriptor len 0x12
-    #print(handle.controlRead(0x80, 0x06, 0x0200, 0x0000, 0x09, timeout=20)) # get descriptor len 0x09
-    #print(handle.controlRead(0x80, 0x06, 0x0200, 0x0000, 0x22, timeout=20)) # get descriptor len 0x22
-    #handle.controlWrite(0x00, 0x01, 0x0001, 0x0000, b"", timeout=20) # clear feature 1
-    #print(handle.controlRead(0x80, 0x00, 0x0000, 0x0000, 0x0002, timeout=20)) # get status
-    #print(handle.controlRead(0x80, 0x06, 0x0200, 0x0000, 0x22, timeout=20)) # get descriptor len 0x22
-    #time.sleep(0.5e-3)
+    time.sleep(0.5e-3)
+    print(handle.controlRead(0x80, 0x06, 0x0100, 0x0000, 0x0040, timeout=20)) # get descriptor len 0x40
+    time.sleep(0.5e-3)
+    print(handle.controlRead(0x80, 0x06, 0x0100, 0x0000, 0x0012, timeout=20)) # get descriptor len 0x12
+    time.sleep(0.5e-3)
+    print(handle.controlRead(0x80, 0x06, 0x0200, 0x0000, 0x0009, timeout=20)) # get descriptor len 0x09
+    time.sleep(0.5e-3)
+    print(handle.controlRead(0x80, 0x06, 0x0200, 0x0000, 0x0022, timeout=20)) # get descriptor len 0x09
+    time.sleep(0.5e-3)
+    print(handle.controlRead(0x80, 0x06, 0x0300, 0x0000, 0x00FF, timeout=20)) # get descriptor len 0x09
+    time.sleep(0.5e-3)
+    print(handle.controlRead(0x80, 0x06, 0x0302, 0x0409, 0x00FF, timeout=20)) # get descriptor len 0x09
+    time.sleep(0.5e-3)
+    print(handle.controlRead(0x80, 0x06, 0x0301, 0x0409, 0x00FF, timeout=20)) # get descriptor len 0x09
+    time.sleep(0.5e-3)
     handle.controlWrite(0x00, 0x09, 0x0001, 0x0000, b"", timeout=20) # set configuration 1 -> mouse front LED ON
-    print(handle.controlRead(0x80, 0x08, 0x0000, 0x0000, 0x0001, timeout=20)) # get configuration 1 -> mouse front LED ON
-    handle.controlWrite(0x00, 0x03, 0x0001, 0x0000, b"", timeout=20) # set feature 1
-    handle.controlWrite(0x00, 0x01, 0x0001, 0x0000, b"", timeout=20) # clear feature 1
-    #handle.controlWrite(0x02, 0x01, 0x0001, 0x0000, b"", timeout=20) # clear feature 1 of endpoint
-    #print(handle.controlRead(0x82, 0x00, 0x0000, 0x0001, 0x0002, timeout=20)) # get endpoint 1 status
-    #print(handle.controlRead(0x82, 0x12, 0x0000, 0x0000, 0x0002, timeout=20)) # get endpoint 1 status
-    #print(handle.controlRead(0x80, 0x0A, 0x0001, 0x0000, 0x0001, timeout=20)) # get interface
-    #print(handle.controlRead(0x80, 0x06, 0x0200, 0x0000, 0x42, timeout=20)) # get descriptor len 0x09
-    #### this will creash: handle.controlWrite(0x00, 0x05, 0x1234, 0x0000, b"", timeout=20) # set address 1234
-    #handle.controlWrite(0x21, 0x0A, 0x0001, 0x0000, b"", timeout=20) # set idle 0
-    #handle.controlWrite(0x21, 0x09, 0x0201, 0x0000, b"", timeout=20) # set protocol 0x200 error
-    #handle.controlWrite(0x21, 0x09, 0x0200, 0x0000, b"\x00", timeout=20) # set protocol 0x200 error
-    print("press now")
-    time.sleep(0.1)
+    time.sleep(0.5e-3)
+    try:
+      print(handle.controlRead(0x81, 0x06, 0x2200, 0x0000, 0x0048, timeout=50)) # get descriptor len 0x48
+    except:
+      print("some err")
+    print("press buttons/move mouse now")
+    time.sleep(1.0)
     for x in range(0,20):
        try:
            print(handle.interruptRead(1,4,timeout=20)) # get report
        except:
            print(x)
-           time.sleep(0.1)
+           time.sleep(100.0e-3)
